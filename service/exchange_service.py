65afe20bd9a5a7c7684046f23f2061e5494d823c
Victor Smirnov <csatom@yandex.ru>
2023-10-19 20:23:46 +0300

    added service file exchange_service

---
+++ service/exchange_service.py
@@ -0,0 +1,11 @@
+from dao.DAO_exchange_repository import DaoExchangeRepository
+
+
+class ExchangeService(DaoExchangeRepository):
+    """
+    Класс для выполнения бизнес-логики получения расчёта перевода определённого количества средств из одной валюты в другую
+    """
+    pass
+    # прямой курс - отдельный метод
+    # обратный курс - отдельный метод
+    # кросс курс через баксы - отдельный метод
