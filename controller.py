d6da8d9333bad989977a2316bb97cca06b65a1a3
Victor Smirnov <csatom@yandex.ru>
2023-10-25 18:09:46 +0300

    add some get handlers

--- controller.py
+++ controller.py
@@ -2,11 +2,15 @@ from http.server import BaseHTTPRequestHandler
 from http.server import HTTPServer
 import urllib.parse
 from config.config import Config
-from get_performer_currencies import GetPerformerCurrencies
-from get_performer_exchange_rates import GetPerformerExchangeRates
-from patch_performer_exchange_rates import PatchPerformerExchangeRates
-from post_performer_currencies import PostPerformerCurrencies
-from post_performer_exchange_rates import PostPerformerExchangeRates
+# from get_performer_currencies import GetPerformerCurrencies
+# from get_performer_exchange_rates import GetPerformerExchangeRates
+# from patch_performer_exchange_rates import PatchPerformerExchangeRates
+# from post_performer_currencies import PostPerformerCurrencies
+# from post_performer_exchange_rates import PostPerformerExchangeRates
+from dao.DAO_currency_repository import DaoCurrencyRepository
+from dao.DAO_exchange_repository import DaoExchangeRepository
+from dto_response.error_response import ErrorResponse
+from view.view import ViewToJSON
 
 
 class HttpRequestHandler(BaseHTTPRequestHandler):
@@ -23,89 +27,159 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
         Метод обрабатывает запросы GET
         :return: ответ на запрос
         """
-        handler = self.choose_get_handler()
-        response_code, query_data = handler.perform_handling()
-        self.send_response(response_code)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(query_data.encode())
-
-    def choose_get_handler(self):
-        """
-        Метод выбирает объект обработчик, который будет отвечать на запрос
-        :return: объект класса обработчика
-        """
-        if self.path == Config.currencies or self.path.startswith(Config.currency):
-            handler = GetPerformerCurrencies(self.path)
-        if self.path == Config.exchangeRates or self.path.startswith(Config.exchangeRate) or self.path.startswith(Config.exchange):
-            handler = GetPerformerExchangeRates(self.path)
-
-        return handler
-
-    def do_POST(self):
-        """
-        Метод обрабатывает запросы POST
-        :return: ответ на запрос
-        """
-        data_dict = self.get_form_fields()
-        handler = self.choose_post_handler()
-        response_code, query_data = handler.perform_handling(data_dict)
-        self.send_response(response_code)
-        self.send_header('Content-type', 'application/json')
-        self.end_headers()
-        self.wfile.write(query_data.encode())
-
-    def get_form_fields(self):
-        """
-        Метод возвращает словарь с данными полей формы (x-www-form-urlencoded)
-        :return: dict с данными, которые были переданы по x-www-form-urlencoded
-        """
-        content_length = int(self.headers['Content-Length'])
-        post_data = self.rfile.read(content_length).decode('utf-8')
-        data_pieces = post_data.split("&")
-        data_dict = {}
-        for piece in data_pieces:
-            key, value = piece.split("=")
-            # Декодируем значение из URL-кодирования, если необходимо
-            value = urllib.parse.unquote(value)
-            data_dict[key] = value
-
-        return data_dict
-
-    def choose_post_handler(self):
-        """
-        Метод выбирает обработчик в зависимости от пути запроса
-        :return: объект класса обработчика
-        """
         if self.path == Config.currencies:
-            handler = PostPerformerCurrencies(self.path)
-        if self.path == Config.exchangeRates:
-            handler = PostPerformerExchangeRates(self.path)
+            response_code, json_data = self.get_currencies()
+        elif self.path.startswith(Config.currency):
+            response_code, json_data = self.get_currency(self.path)
+        elif self.path == Config.exchangeRates:
+            response_code, json_data = self.get_exchange_rates()
 
-        return handler
 
-    def do_PATCH(self):
-        """
-        Метод обрабатывает запросы PATCH
-        :return: ответ на запрос
-        """
-        data_dict = self.get_form_fields()
-        handler = self.choose_patch_handler()
-        response_code, query_data = handler.perform_handling(data_dict)
         self.send_response(response_code)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write(query_data.encode())
+        self.wfile.write(json_data.encode())
 
-    def choose_patch_handler(self):
+    def get_currencies(self):
         """
-        Метод выбирает обработчик PATCH запроса
-        :return: функция обработчик
+        Метод возвращает данные по запросу GET /currencies
+        :return: кортеж из двух элементов:
+        0 - индекс - response_code
+        1 - индекс - json_data
         """
-        if self.path.startswith(Config.exchangeRate):
-            handler = PatchPerformerExchangeRates(self.path)
+        view = ViewToJSON()  # объект класса ViewToJSON для представления
+        handler = DaoCurrencyRepository()
+        response = handler.find_all()
+        if isinstance(response, ErrorResponse):
+            response_code = response.code
+            response = {"message": response.message}
+            json_data = view.dumps_to_json(response)
+        else:
+            response_code = 200
+            response = view.view_all_currencies(response)
+            json_data = view.dumps_to_json(response)
+        return (response_code, json_data)
 
-        return handler
+    def get_currency(self, path):
+        """
+        Метод возвращает данные по запросу GET /currency/
+        :param path: путь запроса
+        :return: кортеж из двух элементов:
+        0 - индекс - response_code
+        1 - индекс - json_data
+        """
+        view = ViewToJSON()  # объект класса ViewToJSON для представления
+        handler = DaoCurrencyRepository()
+        splitted_path = path.split("/")
+        currency_code = splitted_path[-1]
+        response = handler.find_by_code(currency_code)
+        if isinstance(response, ErrorResponse):
+            response_code = response.code
+            response = {"message": response.message}
+            json_data = view.dumps_to_json(response)
+        else:
+            response_code = 200
+            response = view.view_currency(response)
+            json_data = view.dumps_to_json(response)
+        return (response_code, json_data)
+
+    def get_exchange_rates(self):
+        """
+        Метод возвращает данные по запросу GET /exchangeRates
+        :return: кортеж из двух элементов:
+        0 - индекс - response_code
+        1 - индекс - json_data
+        """
+        view = ViewToJSON()  # объект класса ViewToJSON для представления
+        handler = DaoExchangeRepository()
+        response = handler.find_all()
+        if isinstance(response, ErrorResponse):
+            response_code = response.code
+            response = {"message": response.message}
+            json_data = view.dumps_to_json(response)
+        else:
+            response_code = 200
+            response = view.view_all_exchange_rates(response)
+            json_data = view.dumps_to_json(response)
+        return (response_code, json_data)
+
+
+    # def choose_get_handler(self):
+    #     """
+    #     Метод выбирает объект обработчик, который будет отвечать на запрос
+    #     :return: объект класса обработчика
+    #     """
+    #     if self.path == Config.currencies or self.path.startswith(Config.currency):
+    #         handler = GetPerformerCurrencies(self.path)
+    #     if self.path == Config.exchangeRates or self.path.startswith(Config.exchangeRate) or self.path.startswith(Config.exchange):
+    #         handler = GetPerformerExchangeRates(self.path)
+    #
+    #     return handler
+    #
+    # def do_POST(self):
+    #     """
+    #     Метод обрабатывает запросы POST
+    #     :return: ответ на запрос
+    #     """
+    #     data_dict = self.get_form_fields()
+    #     handler = self.choose_post_handler()
+    #     response_code, query_data = handler.perform_handling(data_dict)
+    #     self.send_response(response_code)
+    #     self.send_header('Content-type', 'application/json')
+    #     self.end_headers()
+    #     self.wfile.write(query_data.encode())
+    #
+    # def get_form_fields(self):
+    #     """
+    #     Метод возвращает словарь с данными полей формы (x-www-form-urlencoded)
+    #     :return: dict с данными, которые были переданы по x-www-form-urlencoded
+    #     """
+    #     content_length = int(self.headers['Content-Length'])
+    #     post_data = self.rfile.read(content_length).decode('utf-8')
+    #     data_pieces = post_data.split("&")
+    #     data_dict = {}
+    #     for piece in data_pieces:
+    #         key, value = piece.split("=")
+    #         # Декодируем значение из URL-кодирования, если необходимо
+    #         value = urllib.parse.unquote(value)
+    #         data_dict[key] = value
+    #
+    #     return data_dict
+    #
+    # def choose_post_handler(self):
+    #     """
+    #     Метод выбирает обработчик в зависимости от пути запроса
+    #     :return: объект класса обработчика
+    #     """
+    #     if self.path == Config.currencies:
+    #         handler = PostPerformerCurrencies(self.path)
+    #     if self.path == Config.exchangeRates:
+    #         handler = PostPerformerExchangeRates(self.path)
+    #
+    #     return handler
+    #
+    # def do_PATCH(self):
+    #     """
+    #     Метод обрабатывает запросы PATCH
+    #     :return: ответ на запрос
+    #     """
+    #     data_dict = self.get_form_fields()
+    #     handler = self.choose_patch_handler()
+    #     response_code, query_data = handler.perform_handling(data_dict)
+    #     self.send_response(response_code)
+    #     self.send_header('Content-type', 'application/json')
+    #     self.end_headers()
+    #     self.wfile.write(query_data.encode())
+    #
+    # def choose_patch_handler(self):
+    #     """
+    #     Метод выбирает обработчик PATCH запроса
+    #     :return: функция обработчик
+    #     """
+    #     if self.path.startswith(Config.exchangeRate):
+    #         handler = PatchPerformerExchangeRates(self.path)
+    #
+    #     return handler
 
 
 
---
+++ view/view.py
@@ -0,0 +1,65 @@
+import json
+
+from dao.DAO_currency_repository import DaoCurrencyRepository
+from model.currency import Currency
+
+
+class ViewToJSON:
+    """
+    Класс просто берет коллекцию python и возвращает JSON
+    """
+    def dumps_to_json(self, data):
+        """
+        Метод берет какие-либо данные - словарь или список и преобразует их в JSON-объект
+        :param data: словарь или список
+        :return: JSON-объект
+        """
+        json_data = json.dumps(data, indent=4, ensure_ascii=False)
+        return json_data
+
+    def view_all_currencies(self, response):
+        """
+        Метод для обработки данных по всем валютам
+        :param response: это список всех валют - список объектов класса Currency
+        :return: преобразованный список словарей с данными по всем валютам, который потом можно превратить в JSON
+        """
+        response_to_json = []
+        for currency in response:
+            currency_dict = {"id": currency.ID, "name": currency.FullName, "code": currency.Code, "sign": currency.Sign}
+            response_to_json.append(currency_dict)
+
+        return response_to_json
+
+    def view_currency(self, response):
+        """
+        Метод для обработки данных по одной валюте
+        :param response: объект класса Currency
+        :return: словарь с данными о валюте или словарь с ошибкой
+        """
+        currency_dict = {"id": response.ID, "name": response.FullName, "code": response.Code, "sign": response.Sign}
+        return currency_dict
+
+    def view_all_exchange_rates(self, response):
+        """
+        Метод для обработки данных по всем обменным курсам
+        :param response: это список всех обменных курсов - список объектов класса ExchangeRate
+        :return: преобразованный список словарей с данными по всем валютам, который потом можно превратить в JSON
+        """
+        obj_dao_currency = DaoCurrencyRepository() # объект класса DaoCurrencyRepository для поиска валюты по ID
+        response_to_json = []
+        for exchange_rate in response:
+            exchange_rate_id = exchange_rate.ID
+            BaseCurrencyId = exchange_rate.BaseCurrencyId
+            TargetCurrencyId = exchange_rate.TargetCurrencyId
+            Rate = exchange_rate.Rate
+            baseCurrency = obj_dao_currency.find_by_id(BaseCurrencyId)
+            baseCurrency = self.view_currency(baseCurrency)
+            targetCurrency = obj_dao_currency.find_by_id(TargetCurrencyId)
+            targetCurrency = self.view_currency(targetCurrency)
+            exchange_rate_dict = {"id": exchange_rate_id,
+                                  "baseCurrency": baseCurrency,
+                                  "targetCurrency": targetCurrency,
+                                  "rate": Rate}
+            response_to_json.append(exchange_rate_dict)
+
+        return response_to_json
