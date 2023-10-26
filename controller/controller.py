e6edfb12e4b893a753d014bdc3b38b5d709c7151
Victor Smirnov <csatom@yandex.ru>
2023-10-26 17:48:47 +0300

    added do_POST method

--- controller/controller.py
+++ controller/controller.py
@@ -1,4 +1,5 @@
 import sys
+import urllib
 
 from service.exchange_service import ExchangeService
 
@@ -30,15 +31,15 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
         Метод обрабатывает запросы GET
         :return: ответ на запрос
         """
-        if self.path == Config.currencies:                                  # запрос в GET /currencies
+        if self.path == Config.currencies:                                  # запрос GET /currencies
             response_code, json_data = self.get_currencies()
-        elif self.path.startswith(Config.currency):                         # запрос в GET /currency/
+        elif self.path.startswith(Config.currency):                         # запрос GET /currency/
             response_code, json_data = self.get_currency(self.path)
-        elif self.path == Config.exchangeRates:                             # запрос в GET /exchangeRates
+        elif self.path == Config.exchangeRates:                             # запрос GET /exchangeRates
             response_code, json_data = self.get_exchange_rates()
-        elif self.path.startswith(Config.exchangeRate):                     # запрос в GET /exchangeRate/
+        elif self.path.startswith(Config.exchangeRate):                     # запрос GET /exchangeRate/
             response_code, json_data = self.get_exchange_rate(self.path)
-        elif self.path.startswith(Config.exchange):                         # запрос в GET /exchange?
+        elif self.path.startswith(Config.exchange):                         # запрос GET /exchange?
             response_code, json_data = self.get_exchange(self.path)
 
         self.send_response(response_code)
@@ -162,36 +163,39 @@ class HttpRequestHandler(BaseHTTPRequestHandler):
 
 
 
-    # def do_POST(self):
-    #     """
-    #     Метод обрабатывает запросы POST
-    #     :return: ответ на запрос
-    #     """
-    #     data_dict = self.get_form_fields()
-    #     handler = self.choose_post_handler()
-    #     response_code, query_data = handler.perform_handling(data_dict)
-    #     self.send_response(response_code)
-    #     self.send_header('Content-type', 'application/json')
-    #     self.end_headers()
-    #     self.wfile.write(query_data.encode())
-    #
-    # def get_form_fields(self):
-    #     """
-    #     Метод возвращает словарь с данными полей формы (x-www-form-urlencoded)
-    #     :return: dict с данными, которые были переданы по x-www-form-urlencoded
-    #     """
-    #     content_length = int(self.headers['Content-Length'])
-    #     post_data = self.rfile.read(content_length).decode('utf-8')
-    #     data_pieces = post_data.split("&")
-    #     data_dict = {}
-    #     for piece in data_pieces:
-    #         key, value = piece.split("=")
-    #         # Декодируем значение из URL-кодирования, если необходимо
-    #         value = urllib.parse.unquote(value)
-    #         data_dict[key] = value
-    #
-    #     return data_dict
-    #
+    def do_POST(self):
+        """
+        Метод обрабатывает запросы POST
+        :return: ответ на запрос
+        """
+        data_dict = self.get_form_fields()
+
+        if self.path == Config.currencies:                          # запрос POST /currencies
+            response_code, json_data = self.post_currencies()
+        elif self.path == Config.exchangeRates:                     # запрос POST /exchangeRates
+            response_code, json_data = self.post_exchange_rates()
+
+        self.send_response(response_code)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json_data.encode())
+
+    def get_form_fields(self):
+        """
+        Метод возвращает словарь с данными полей формы (x-www-form-urlencoded)
+        :return: dict с данными, которые были переданы по x-www-form-urlencoded
+        """
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        data_pieces = post_data.split("&")
+        data_dict = {}
+        for piece in data_pieces:
+            key, value = piece.split("=")
+            value = urllib.parse.unquote(value) # Декодируем значение из URL-кодирования, если необходимо
+            data_dict[key] = value
+
+        return data_dict
+
     # def choose_post_handler(self):
     #     """
     #     Метод выбирает обработчик в зависимости от пути запроса
